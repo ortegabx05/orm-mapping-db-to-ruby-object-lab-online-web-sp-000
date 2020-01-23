ff55f6fd6ab33188a65e6ccbf0f880762e247e75
jessi ortega <ortegabx05@gmail.com>
2020-01-23 05:27:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,8 @@ end
   def self.first_x_students_in_grade_10(x)
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
     DB[:conn].execute(sql, x).map do |row|
-    
+      self.new_from_db(row)
+    end
   end
   
   def save
