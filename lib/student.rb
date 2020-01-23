dbd563aacbe631b7fac7b40aabf6fb4abb3d95a0
jessi ortega <ortegabx05@gmail.com>
2020-01-23 04:54:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,13 @@ end
     end.first
   end
   
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+      DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end	  
+  
   def save
     sql = <<-SQL
     INSERT INTO students (name, grade) 
