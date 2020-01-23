f24c4e98efbce39e24961e87f78b7225921b3444
jessi ortega <ortegabx05@gmail.com>
2020-01-23 04:47:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ end
   def self.students_below_12th_grade
     sql = <<-SQL
     SELECT * FROM students WHERE students.grade < 12
-
+      SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
