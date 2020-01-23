b7ef428bc970ddbb8b827eac8b2e6453b64e2840
jessi ortega <ortegabx05@gmail.com>
2020-01-23 05:16:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,18 +36,11 @@ end
     end
   end
   
-  
-    def self.first_x_students_in_grade_10(x)
-    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
-    first_x_student = DB[:conn].execute(sql, x)
-    self.new_from_db(first_x_student)
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    first_student_row = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
   end
-
- # def self.first_student_in_grade_10
-  #  sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
-   # first_student_row = DB[:conn].execute(sql)[0]
-   # self.new_from_db(first_student_row)
- # end
   
   def self.all_students_in_grade_X(x)
     sql = "SELECT * FROM students WHERE grade = ?"
